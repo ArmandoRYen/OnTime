11d3e15745aea0ee08ddebc583b5827f17ff42fd
Armando Ruiz <armandoruiz.upc@gmail.com>
2021-09-25 18:55:07 -0500

    Modificacion Evento

--- src/main/java/pe/edu/upc/controller/EventoController.java
+++ src/main/java/pe/edu/upc/controller/EventoController.java
@@ -9,12 +9,17 @@ import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.ChartSeries;
+import org.primefaces.model.chart.HorizontalBarChartModel;
+
 import pe.edu.upc.entity.Evento;
 import pe.edu.upc.service.IEventoService;
 
 import pe.edu.upc.entity.TipoEvento;
 import pe.edu.upc.service.ITipoEventoService;
-
+import pe.edu.upc.serviceimpl.LoginService;
 import pe.edu.upc.entity.Persona;
 import pe.edu.upc.service.IPersonaService;
 
@@ -29,33 +34,39 @@ public class EventoController implements Serializable {
 
 	@Inject
 	private IEventoService evService;
-	
+
 	@Inject
 	private IPersonaService pService;
 
 	private TipoEvento tipoevento;
 	List<TipoEvento> listaTipoEventos;
-	
+
 	private Evento evento;
 	List<Evento> listaEventos;
-	
+
 	private Persona persona;
 	List<Persona> listaPersonas;
+
+	@Inject
+	private LoginService loginService;
+
+	private HorizontalBarChartModel horizontalBarModel;
 
 	@PostConstruct
 	public void init() {
 		this.listaTipoEventos = new ArrayList<TipoEvento>();
 		this.tipoevento = new TipoEvento();
-		
+
 		this.listaEventos = new ArrayList<Evento>();
 		this.evento = new Evento();
-		
+
 		this.listaPersonas = new ArrayList<Persona>();
 		this.persona = new Persona();
 
 		this.listarTipoEvento();
 		this.listarEvento();
 		this.listarPersona();
+		this.createHorizontalBarModel();
 	}
 
 	public String nuevoEvento() {
@@ -63,10 +74,29 @@ public class EventoController implements Serializable {
 		return "evento.xhtml";
 	}
 
-	public void insertar() {
-		evService.insertar(evento);
-		limpiarEvento();
-		this.listarEvento();
+	public void insertarEvento() {
+		evento.setIdEvento(0);
+		Persona personaLogin = loginService.getPersona();
+		if (personaLogin != null) {
+			evento.setPersona(personaLogin);
+			evService.insertar(evento);
+			limpiarEvento();
+			this.listarEvento();
+		}
+	}
+
+	public void insertarTipoEvento() {
+		tipoevento.setIdTipoEvento(0);
+		Persona personaLogin = loginService.getPersona();
+		if (personaLogin != null) {
+			System.out.println(personaLogin.getNombrePersona());
+			tipoevento.setPersona(personaLogin);
+			teService.insertar(tipoevento);
+			limpiarEvento();
+			this.listarTipoEvento();
+		} else {
+			System.out.println("null");
+		}
 	}
 
 	public void limpiarEvento() {
@@ -74,13 +104,24 @@ public class EventoController implements Serializable {
 	}
 
 	public void listarTipoEvento() {
-		listaTipoEventos = teService.listar();
+		Persona personaLogin = loginService.getPersona();
+		if (personaLogin == null) {
+			listaTipoEventos = teService.listar();
+		} else {
+			listaTipoEventos = teService.listarPorNombre(personaLogin.getNombrePersona());
+		}
+
 	}
 
 	public void listarEvento() {
-		listaEventos = evService.listar();
+		Persona personaLogin = loginService.getPersona();
+		if (personaLogin == null) {
+			listaEventos = evService.listar();
+		} else {
+			listaEventos = evService.listarPorNombre(personaLogin.getNombrePersona());
+		}
 	}
-	
+
 	public void listarPersona() {
 		listaPersonas = pService.listar();
 	}
@@ -88,7 +129,7 @@ public class EventoController implements Serializable {
 	public void limpiarTipoEvento() {
 		this.init();
 	}
-	
+
 	public void limpiarPersona() {
 		this.init();
 	}
@@ -146,7 +187,44 @@ public class EventoController implements Serializable {
 		this.listaPersonas = listaPersonas;
 	}
 
-	
-	
+	public HorizontalBarChartModel getHorizontalBarModel() {
+		return horizontalBarModel;
+	}
+
+	public void setHorizontalBarModel(HorizontalBarChartModel horizontalBarModel) {
+		this.horizontalBarModel = horizontalBarModel;
+	}
+
+	private void createHorizontalBarModel() {
+		horizontalBarModel = new HorizontalBarChartModel();
+
+		ChartSeries eventos = new ChartSeries();
+		eventos.setLabel("Eventos");
+		
+		int cont = 0;
+		for(int i = 0; i < listaTipoEventos.size(); i++) {
+			for(int j = 0; j < listaEventos.size(); j++) {
+				if(listaTipoEventos.get(i).getIdTipoEvento() == listaEventos.get(j).getTipoEvento().getIdTipoEvento()) {
+					cont++;
+				}
+			}
+			eventos.set(listaTipoEventos.get(i).getNombreTipoEvento(), cont);
+			cont = 0;
+		}
+
+		horizontalBarModel.addSeries(eventos);
+
+		horizontalBarModel.setTitle("Horizontal and Stacked");
+		horizontalBarModel.setLegendPosition("e");
+		horizontalBarModel.setStacked(true);
+
+		Axis xAxis = horizontalBarModel.getAxis(AxisType.X);
+		xAxis.setLabel("Número de eventos");
+		xAxis.setMin(0);
+		xAxis.setMax(10);
+
+		Axis yAxis = horizontalBarModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Tipo de Evento");
+	}
 
 }
